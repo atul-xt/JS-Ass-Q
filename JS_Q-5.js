01d32e4e48aca42f94be7fb62cb4fa416b68a795
atul-xt <atul.xtt@gmail.com>
2023-07-24 16:42:07 +0530

    Greater among 3 numbers

---
+++ JS_Q-5.js
@@ -0,0 +1,23 @@
+// Qs5. Write a program to find the largest of 3 numbers.
+
+let a = 10, b = 20, c = 30;
+
+if (a > b) {
+    if (a > c) {
+        console.log("A is Greater");
+    }
+    else
+    {
+        console.log("C is Greater");
+    }
+}
+else 
+{
+    if (b > c) {
+        console.log("B is Greater");
+    }
+    else
+    {
+        console.log("C is Greater");
+    }
+}
