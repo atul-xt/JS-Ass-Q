fe579a7802852720ef51235522c551e997d1df44
atul-xt <atul.xtt@gmail.com>
2023-07-24 17:57:11 +0530

    String is blank or not

---
+++ JS_Q-9.js
@@ -0,0 +1,12 @@
+// Qs9. Write a JavaScript program to check whether a string is blank or not.
+
+let str = "";
+
+if (str.length === 0)
+{
+    console.log("String is Blank");
+}
+else
+{
+    console.log("String isn't Blank")
+}
