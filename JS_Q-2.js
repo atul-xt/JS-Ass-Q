4feacd9c318a64262042c4d59408041b8cc67053
atul-xt <atul.xtt@gmail.com>
2023-07-24 15:04:33 +0530

    Using Prompt & Alert

---
+++ JS_Q-2.js
@@ -0,0 +1,10 @@
+// Qs2. Take the user's name & age as input using prompts.
+// Then return back the following statement to the user as an alert (by substituting
+// their name & age) :
+// name is age years old.
+// [Use template Literals to print this sentence]
+
+let userName = prompt("Enter Your Name");
+let userAge = prompt("Enter Your Age");
+
+alert(`${userName} is ${userAge} year's old.`);
