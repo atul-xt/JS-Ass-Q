f86cdb42660fc42abab31c35e97e49dc1c288a5a
atul-xt <atul.xtt@gmail.com>
2023-07-24 18:13:21 +0530

    Given Index is lowerCase or not

---
+++ JS_Q-10.js
@@ -0,0 +1,14 @@
+// Qs10. Write a JavaScript program to test whether the character at the given (character)
+// index is lower case.
+
+let str = "Atul Sadiwal";
+let index = 1;
+
+if ( str[index] === str[index].toLowerCase(str))
+{
+    console.log("Character is Lower Case");
+}
+else
+{
+    console.log("Character isn't Lower Case");
+}
