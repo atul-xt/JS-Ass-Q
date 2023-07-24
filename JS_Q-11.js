ba0c4d8fcf622d77d8296babe9e1f5316e4e0ddc
atul-xt <atul.xtt@gmail.com>
2023-07-24 18:26:05 +0530

    Element exist in an array or not

---
+++ JS_Q-11.js
@@ -0,0 +1,13 @@
+// Qs11. Write a JavaScript program to check if an element exists in an array or not. 
+
+let arr = [1,2,3,4,5,3];
+let element = 2;
+
+if (arr.includes(element))
+{
+    console.log("Element exist in an Array");
+}
+else
+{
+    console.log("Element isn't exist in an Array");
+}
