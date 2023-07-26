3055926f910f6a70472c6b79e988c198fa6f0a9d
atul-xt <atul.xtt@gmail.com>
2023-07-26 14:40:54 +0530

    Modify Object

---
+++ JS_Q-19.js
@@ -0,0 +1,14 @@
+// Qs3. Create an object Person with their name, age and city.
+// Edit their city’s original value to change it to “New York”.
+// Add a new property country and set it to the United States. 
+
+const person = {
+    name : "Atul",
+    age : 20,
+    city : "Indore"
+};
+
+person.city = "New York";
+person.country = "United States";
+
+console.log(person);
