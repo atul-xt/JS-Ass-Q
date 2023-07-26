4741b67a25a2ea84c91294014bc7fdb5394478f2
atul-xt <atul.xtt@gmail.com>
2023-07-26 11:18:52 +0530

    Create an object of Car and access the property

---
+++ JS_Q-18.js
@@ -0,0 +1,11 @@
+// Qs2. Create an object representing a car that stores the following properties for the
+// car: name, model, color.
+// Print the car’s name.
+
+const car = {
+    name: "BMW",
+    model: "Top Class",
+    color: "red"
+};
+
+console.log(car.name);
